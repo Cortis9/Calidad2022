0215ed6dd1d3a1055a240f859e212cd29339a902
cortis9 <alnmige13@gmail.com>
2022-03-05 10:12:47 -0600

    Pruebas Calculadora

---
+++ src/test/java/com/anahuac/calidad/tareas/AllTests.java
@@ -0,0 +1,11 @@
+package com.anahuac.calidad.tareas;
+
+import org.junit.runner.RunWith;
+import org.junit.runners.Suite;
+import org.junit.runners.Suite.SuiteClasses;
+
+@RunWith(Suite.class)
+@SuiteClasses({ CalculadoraTest.class, SumaParametrisadaTest.class })
+public class AllTests {
+
+}
