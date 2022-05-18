3b3fff218a005ebecd8730eef011efc2e4217ae0
Alan Miguel Sesin Abullarade <90735990+Cortis9@users.noreply.github.com>
2022-05-18 14:58:19 -0500

    Update mernCrudTest.java

--- src/test/java/com/anahuac/calidad/funcional/mernCrudTest.java
+++ src/test/java/com/anahuac/calidad/funcional/mernCrudTest.java
@@ -1,135 +1,98 @@
 package com.anahuac.calidad.funcional;
 
+import static org.junit.Assert.*;
+
+import java.util.regex.Pattern;
 import java.util.concurrent.TimeUnit;
 import org.junit.*;
 import org.junit.runners.MethodSorters;
 
 import static org.junit.Assert.*;
+
 import static org.hamcrest.CoreMatchers.*;
 import org.openqa.selenium.*;
 import org.openqa.selenium.chrome.ChromeDriver;
-
+import org.openqa.selenium.support.ui.Select;
 
 import io.github.bonigarcia.wdm.WebDriverManager;
 
+import org.apache.commons.io.FileUtils;
+import java.io.File;
+
+
 @FixMethodOrder(MethodSorters.NAME_ASCENDING)
-public class mernCrudTest {
-
-	  private WebDriver driver;
-	  private String baseUrl;
-	  private boolean acceptNextAlert = true;
-	  private StringBuffer verificationErrors = new StringBuffer();
-	  JavascriptExecutor js;
-
-	  @Before
-	  public void setUp() throws Exception {
+public class MernCrudTest {
+  private WebDriver driver;
+  private String baseUrl;
+  private boolean acceptNextAlert = true;
+  private StringBuffer verificationErrors = new StringBuffer();
+  JavascriptExecutor js;
+  @Before
+  public void setUp() throws Exception {
 		WebDriverManager.chromedriver().setup();
-		driver = new ChromeDriver();
-	    baseUrl = "https://www.google.com/";
-	    driver.manage().timeouts().implicitlyWait(30, TimeUnit.SECONDS);
-	    js = (JavascriptExecutor) driver;
-	  }
+	driver = new ChromeDriver();
+    baseUrl = "https://www.google.com/";
+    driver.manage().timeouts().implicitlyWait(60, TimeUnit.SECONDS);
+    js = (JavascriptExecutor) driver;
+  }
 
-	  @Test
-	  public void ACrear() throws Exception {
-		    driver.get("https://mern-crud.herokuapp.com/");
-		    driver.findElement(By.xpath("//div[@id='root']/div/div[2]/button")).click();
-		    driver.findElement(By.name("name")).click();
-		    driver.findElement(By.name("name")).clear();
-		    driver.findElement(By.name("name")).sendKeys("alan");
-		    driver.findElement(By.name("email")).click();
-		    driver.findElement(By.name("email")).clear();
-		    driver.findElement(By.name("email")).sendKeys("hola@hmai.com");
-		    driver.findElement(By.name("age")).click();
-		    driver.findElement(By.name("age")).clear();
-		    driver.findElement(By.name("age")).sendKeys("12");
-		    driver.findElement(By.xpath("(.//*[normalize-space(text()) and normalize-space(.)='Gender'])[2]/following::div[1]")).click();
-		    driver.findElement(By.xpath("(.//*[normalize-space(text()) and normalize-space(.)='Male'])[1]/following::div[2]")).click();
-		    driver.findElement(By.xpath("(.//*[normalize-space(text()) and normalize-space(.)='Woah!'])[1]/following::button[1]")).click();
-	    
-		    pause(3000);
-		    
-		    WebElement etiquetaEsperada = driver.findElement(By.xpath("/html/body/div[2]/div/div[2]/form/div[4]/div/div"));
-	    
-	    String text = etiquetaEsperada.getText();
-	    
-	    
-	    assertThat("Nice one!", is(text));
-	    
-	  }
-	  
-	  @Test
-	  public void BModificar() throws Exception {
-		  
-		    driver.get("https://mern-crud.herokuapp.com/");
-		    driver.findElement(By.xpath("//div[@id='root']/div/div[2]/table/tbody/tr/td[5]/button")).click();
-		    driver.findElement(By.name("name")).click();
-		    driver.findElement(By.name("name")).clear();
-		    driver.findElement(By.name("name")).sendKeys("rene");
-		    driver.findElement(By.xpath("(.//*[normalize-space(text()) and normalize-space(.)='Woah!'])[1]/following::button[1]")).click();
-		  
-		
-		    
-        pause(3000);
-		    
-	    WebElement etiquetaEsperada = driver.findElement(By.xpath("/html/body/div[2]/div/div[2]/form/div[4]/div/p"));
-	    
-	    String text = etiquetaEsperada.getText();
-	    
-	    
-	    assertThat("Successfully updated!", is(text));
-		  
-	  }
-	  
-	  @Test
-	  public void CEncontrar() throws Exception {
-		  
-		  driver.get("https://mern-crud.herokuapp.com/");
-		  
-		  pause(3000);
-		    
-		  WebElement etiquetaEsperada = driver.findElement(By.xpath("//*[@id=\"root\"]/div/div[2]/table/tbody/tr[1]/td[1]"));
-		    
-		  String text = etiquetaEsperada.getText();
-		    
-		    
-		  assertThat("Rene", is(text));
-		  
-		  
-		  
-	  }
-	  
-	  
-	  @Test
-	  public void DEliminar() throws Exception {
-		  
-		  driver.get("https://mern-crud.herokuapp.com/");
-		    driver.findElement(By.xpath("//div[@id='root']/div/div[2]/table/tbody/tr/td[5]/button[2]")).click();
-		    WebElement etiquetaEsperada = driver.findElement(By.xpath("/html/body/div[2]/div/div[1]"));
-		    pause(3000);
-		    
-		    driver.findElement(By.xpath("(.//*[normalize-space(text()) and normalize-space(.)='Rene'])[2]/following::button[1]")).click();
+ 
+  @Test
+  public void Test_1Add() throws Exception {
+    driver.get("https://mern-crud.herokuapp.com/");
+    driver.findElement(By.xpath("//div[@id='root']/div/div[2]/button")).click();
+    driver.findElement(By.name("name")).click();
+    driver.findElement(By.name("name")).clear();
+    driver.findElement(By.name("name")).sendKeys("H");
+    driver.findElement(By.name("email")).click();
+    driver.findElement(By.name("email")).clear();
+    driver.findElement(By.name("email")).sendKeys("correo@a.com");
+    driver.findElement(By.name("age")).click();
+    driver.findElement(By.name("age")).clear();
+    driver.findElement(By.name("age")).sendKeys("44");
+    driver.findElement(By.xpath("(.//*[normalize-space(text()) and normalize-space(.)='Gender'])[2]/following::div[1]")).click();
+    driver.findElement(By.xpath("(.//*[normalize-space(text()) and normalize-space(.)='Male'])[2]/following::div[1]")).click();
+    driver.findElement(By.xpath("(.//*[normalize-space(text()) and normalize-space(.)='Woah!'])[1]/following::button[1]")).click();
+    pause(4000);
+    //Verify(assert)
+    WebElement  etiquetaEsperada = driver.findElement(By.xpath("/html/body/div[2]/div/div[2]/form/div[4]/div/div"));
+    String textoResultado = etiquetaEsperada.getText();
+    assertThat("Nice one!",is(textoResultado));
+  }
 
-		
-		    
-		    String text = etiquetaEsperada.getText();
-		    
-		    
-		    assertThat("Rene", is(not(text)));
-		  
-	  }
-	  
+  @Test
+  public void Test_2Update() throws Exception {
+	  driver.get("https://mern-crud.herokuapp.com/");
+	    driver.findElement(By.xpath("/html/body/div/div/div[2]/table/tbody/tr/td[5]/button[1]")).click();
+	    driver.findElement(By.name("name")).click();
+	    driver.findElement(By.name("name")).clear();
+	    pause(1000);
+	    driver.findElement(By.name("name")).click();
+	    driver.findElement(By.name("name")).sendKeys("Update");
+	    driver.findElement(By.xpath("(.//*[normalize-space(text()) and normalize-space(.)='Woah!'])[1]/following::button[1]")).click();
+	    pause(5000);
+	    //Verify(assert)
+	WebElement  updateEsperado = driver.findElement(By.xpath("/html/body/div[2]/div/div[2]/form/div[4]/div/p"));
+	String texto = updateEsperado.getText();
+	assertThat("Successfully updated!",is(texto));	    
+  }
 
-	  @After
-	  public void tearDown() throws Exception {
-	    driver.quit();
-	    String verificationErrorString = verificationErrors.toString();
-	    if (!"".equals(verificationErrorString)) {
-	      fail(verificationErrorString);
+  @Test
+  public void Test_3Eliminar() throws Exception {
+	driver.get("https://mern-crud.herokuapp.com/");
+	driver.findElement(By.xpath("//div[@id='root']/div/div[2]/table/tbody/tr/td[5]/button[2]")).click();
+	driver.findElement(By.xpath("(.//*[normalize-space(text()) and normalize-space(.)='Delete User'])[1]/following::p[1]")).click();
+	driver.findElement(By.xpath("(.//*[normalize-space(text()) and normalize-space(.)='Hupdate'])[2]/following::button[1]")).click();
+	driver.findElement(By.xpath("/html/body/div[2]/div/div[3]/button[1]")).click();
+	driver.findElement(By.xpath("//body")).click();
+	    pause(5000);
+	//Verificar(assert)
+	WebElement  etiquetaEsperada = driver.findElement(By.xpath("/html/body/div/div/div[2]/table/tbody"));
+	String textoResultado = etiquetaEsperada.getText();
+	assertThat("null",is(not(textoResultado)));
 	    }
-	  }
-
-	  private boolean isElementPresent(By by) {
+  
+  private boolean isElementPresent(By by) {
 	    try {
 	      driver.findElement(by);
 	      return true;
@@ -137,36 +100,47 @@ public class mernCrudTest {
 	      return false;
 	    }
 	  }
+  @After
+  public void tearDown() throws Exception {
+    driver.quit();
+    String verificationErrorString = verificationErrors.toString();
+    if (!"".equals(verificationErrorString)) {
+      fail(verificationErrorString);
+    }
+  }
+  
+  
+  private boolean isAlertPresent() {
+    try {
+      driver.switchTo().alert();
+      return true;
+    } catch (NoAlertPresentException e) {
+      return false;
+    }
+  }
+  
+  private void pause(long mils) {
+	  try {
+		  Thread.sleep(mils);
+	  }catch(Exception e){
+		  e.printStackTrace();
+	  }
+  }
 
-	  private boolean isAlertPresent() {
-	    try {
-	      driver.switchTo().alert();
-	      return true;
-	    } catch (NoAlertPresentException e) {
-	      return false;
-	    }
-	  }
 
-	  private String closeAlertAndGetItsText() {
-	    try {
-	      Alert alert = driver.switchTo().alert();
-	      String alertText = alert.getText();
-	      if (acceptNextAlert) {
-	        alert.accept();
-	      } else {
-	        alert.dismiss();
-	      }
-	      return alertText;
-	    } finally {
-	      acceptNextAlert = true;
-	    }
-	  }
-	  
-	  private void pause(long mils) {
-		  try {
-			  Thread.sleep(mils);
-		  }catch(Exception e){
-			  e.printStackTrace();
-		  }
-	  }
+  private String closeAlertAndGetItsText() {
+    try {
+      Alert alert = driver.switchTo().alert();
+      String alertText = alert.getText();
+      if (acceptNextAlert) {
+        alert.accept();
+      } else {
+        alert.dismiss();
+      }
+      return alertText;
+    } finally {
+      acceptNextAlert = true;
+    }
+  }
 }
+
