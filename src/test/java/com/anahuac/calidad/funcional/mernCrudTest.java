981d2bff488c7adcdc5daed75b6a274091e984ce
Alan Miguel Sesin Abullarade <90735990+Cortis9@users.noreply.github.com>
2022-05-21 09:05:19 -0500

    Update mernCrudTest.java

--- src/test/java/com/anahuac/calidad/funcional/mernCrudTest.java
+++ src/test/java/com/anahuac/calidad/funcional/mernCrudTest.java
@@ -121,9 +121,10 @@ public class mernCrudTest {
 	  public void DEliminar() throws Exception {
 		  
 		  driver.get("https://mern-crud.herokuapp.com/");
+		  pause(5000);
 		    driver.findElement(By.xpath("//div[@id='root']/div/div[2]/table/tbody/tr/td[5]/button[2]")).click();
 		    WebElement etiquetaEsperada = driver.findElement(By.xpath("/html/body/div[2]/div/div[1]"));
-		    pause(3000);
+		    pause(5000);
 		    
 		    driver.findElement(By.xpath("(.//*[normalize-space(text()) and normalize-space(.)='Rene'])[2]/following::button[1]")).click();
 
